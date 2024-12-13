f8132f4726b85a219258cfe99cde3e57e4316f86
Sarb <web2it@hotmail.co.uk>
2024-12-13 17:41:42 +0530

    small changes

--- src/services/product.js
+++ src/services/product.js
@@ -8,16 +8,16 @@ const getProducts = async (req, res) => {
   try {
     const productList = await product.find({}, 'Price Name MRP Description Quantity StockQuantity ImageUrl').limit(3)
     //res.status(200).send(productList);
-    const productLists = productList.map(product => ({
-      Price: product.Price,
-      Name: product.Name,
-      MRP: product.MRP,
-      Description: product.Description,
-      Quantity: product.Quantity,
-      StockQuantity: product.StockQuantity,
-      ImageUrl: cloud.url(product.ImageUrl) 
-  }));
-  res.status(200).json(productLists);
+    // const productLists = productList.map(product => ({
+    //   Price: product.Price,
+    //   Name: product.Name,
+    //   MRP: product.MRP,
+    //   Description: product.Description,
+    //   Quantity: product.Quantity,
+    //   StockQuantity: product.StockQuantity,
+    //   ImageUrl: "https://res.cloudinary.com/dhb4tjyau/image/upload/v1/"+product.ImageUrl
+ // }));
+  res.status(200).json(productList);
   } catch (error) {
     res.status(400).json(error);
   }
